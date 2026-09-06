12606f62105b64c435a059908376e93ddbf17535
masproo22 <m.abbaszadeh2002@gmail.com>
2026-09-06 20:00:48 +0330

    test: somthings

--- Program.cs
+++ Program.cs
@@ -1,8 +1,42 @@
 using backend.common.data;
 using backend.Extensions;
 using backend.hubs.chat;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
+
+////////////////////////////////////////////////////////////
+var connectionString =
+    builder.Configuration.GetConnectionString("DB_URL")
+    ?? throw new InvalidOperationException(
+        "ConnectionStrings:DB_URL is missing.");
+
+var sqlInfo = new SqlConnectionStringBuilder(connectionString);
+
+Console.WriteLine(
+    "SQL configuration: Server={Server}, Database={Database}, User={User}",
+    sqlInfo.DataSource,
+    sqlInfo.InitialCatalog,
+    sqlInfo.UserID);
+////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 builder.Services.AddApi(builder.Configuration);
 
